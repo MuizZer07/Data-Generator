6279ef360c2d4cf18d2f4b8e13bae52f6938008f
MuizZer07 <muizxzer07@gmail.com>
2019-02-21 03:37:06 +0600

    Present, permanent address added

--- data_generator.py
+++ data_generator.py
@@ -48,7 +48,53 @@ class Data_Generator():
         self.new_generated_DOB = []
         self.new_generated_emails = []
         self.new_generated_mobile_no = []
+        self.new_generated_permanent_address = []
+        self.new_generated_present_address = []
 
+        self.divisions = {
+           1: "Barisal",
+           2: "Chittagong",
+           3: "Dhaka",
+           4: "Khulna",
+           5: "Rajshahi",
+           6: "Mymensingh",
+           7: "Rangpur",
+           8: "Sylhet",  
+        }
+        
+        self.districts = {
+            1: ['Barisal', 'Barguna', 'Bhola', 'Jhalokati', 'Patuakhali', 'Pirojpur'],
+            2: ['Brahmanbaria', 'Comilla', 'Chandpur', 'Lakshmipur', 'Noakhali', 'Feni'],
+            3: ['Dhaka', 'Gazipur', 'Kishoreganj', 'Manikganj', 'Narayanganj', 'Gopalganj'],    
+            4: ['Bagerhat', 'Chuadanga', 'Jessore', 'Jhenaidah', 'Khulna', 'Kushtia'],
+            5: ['Bogura', 'Chapainawabganj', 'Joypurhat', 'Naogaon', 'Natore', 'Rajshahi'],
+            6: ['Jamalpur', 'Mymensingh', 'Netrokona', 'Sherpur'],
+            7: ['Thakurgaon', 'Rangpur', 'Panchagarh', 'Nilphamari', 'Lalmonirhat', 'Kurigram'],
+            8: ['Habiganj', 'Moulvibazar', 'Sunamganj', 'Sylhet'],
+        }
+        
+        self.thanas = {
+            'Dhaka' : ['Motijheel', 'Mirpur', 'Badda', 'Kotwali', 'Tejgaon'],
+            'Chittagong' : ['Rouzan', 'Patiya', 'Mirsharai', 'Barura', 'Fatikchhari'],
+            'Barisal' : ['Amtali', 'Betagi', 'Patharghata', 'Agailzhara', 'Babuganj'],
+            'Khulna' : ['Chitalmari', 'Fakirhat', 'Kachua', 'Mollahat', 'Rampal'],
+            'Rajshahi' : ['Dhunat', 'Gabtoli', 'kalai', 'Ahsanganj', 'Nitpur'],
+            'Mymensingh' : ['Bhaluka', 'Shorishabari', 'Gaforgaon', 'Haluaghat', 'Muktagachha'],
+            'Rangpur': ['Kaunia', 'Gangachara', 'Mithapukur', 'Saidpur', 'Jaldhaka'],
+            'Sylhet' : ['Kalauk', 'Bahubal', 'Madhabpur', 'Nabiganj', 'Kulaura'],
+        }
+        
+        self.villages = {
+            'Dhaka' : ['Bagra', 'Dingamanik', 'Haridaspur', 'Goalgram', 'Fardabad'],
+            'Chittagong' : ['Baria', 'Aitpara', 'Dhumdumia', 'Ramnagar', 'Harbang '],
+            'Barisal' : ['Durga Sagar', 'Gabkhan', 'Ratnopur', 'Goila', 'Kathira'],
+            'Khulna' : ['Payari', 'Rajapur', 'Sarankhola', 'Dumuria', 'Rupsa'],
+            'Rajshahi' : ['Nagar', 'Bhatahar', 'Paikpara', 'Dhopapara', 'Haribhanga'],
+            'Mymensingh' : ['Ujalhati', 'Trishal', 'Phulpur', 'Bahadurpur', 'Nandail'],
+            'Rangpur': ['Badarganj ', 'Palash Kandi', 'Panchanan Barma', 'Mistry Para', 'Dahala'],
+            'Sylhet' : ['Jahidpur', 'Sujaul', 'Balaganj', 'Jotyintapur', 'Sagarnal '],
+        }
+        
     def get_data(self):
         Data = {'Name': self.new_generated_names,
                 'Sex': self.new_generated_sex,
@@ -58,16 +104,17 @@ class Data_Generator():
                 'Date of Birth': self.new_generated_DOB,
                 'Email Address': self.new_generated_emails,
                 'Mobile No. (+880)': self.new_generated_mobile_no,
+                'Permanent Address': self.new_generated_permanent_address,
+                'Present Address': self.new_generated_present_address,
                 }
         columns = ['Name', 'Sex', 'Religion', 'Marital Status', 'Profession', 'Date of Birth', \
-                   'Email Address', 'Mobile No. (+880)']
+                   'Email Address', 'Mobile No. (+880)', 'Permanent Address', 'Present Address']
 
         return Data, columns
 
     def generate_profile(self, first_names, middle_names, last_names, gender, religion):
         for firstname in first_names:
             for lastname in last_names:
-                
                 if middle_names != None:
                     for middlename in middle_names:
                         self.new_generated_names.append(firstname + " " + middlename + " " + lastname)
@@ -78,6 +125,8 @@ class Data_Generator():
                         self.new_generated_DOB.append(self.generate_DOB())
                         self.new_generated_emails.append(firstname.lower() + "." + lastname.lower() + self.email[random.randint(1, 3)])
                         self.new_generated_mobile_no.append(self.generate_phone_no())
+                        self.new_generated_permanent_address.append(self.generate_permanent_address())
+                        self.new_generated_present_address.append(self.generate_present_address())
                 else:
                     self.new_generated_names.append(firstname + " " + lastname)
                     self.new_generated_sex.append(gender)
@@ -87,8 +136,33 @@ class Data_Generator():
                     self.new_generated_DOB.append(self.generate_DOB())
                     self.new_generated_emails.append(firstname.lower() + "." + lastname.lower() + self.email[random.randint(1, 3)])
                     self.new_generated_mobile_no.append(self.generate_phone_no())
+                    self.new_generated_permanent_address.append(self.generate_permanent_address())
+                    self.new_generated_present_address.append(self.generate_present_address())
                 
-
+    def generate_present_address(self):
+        house_no = random.randint(1, 70)
+        road_no = random.randint(1, 20)
+        ward_no = random.randint(1, 45)
+        div = random.randint(1, 7)
+        city = random.choice(self.districts[div])
+        thana = random.choice(self.thanas[self.divisions[div]])
+        dist = city
+        division = self.divisions[div]
+        
+        return "House no. " + str(house_no) +  ", Road no. " + str(road_no) + ", Ward no. " + str(ward_no) + \
+                ", Thana: " + thana + ", City: " + city +", District: " + dist + ", Division: " + division + "."
+    
+    def generate_permanent_address(self):
+        ward_no = random.randint(1, 45)
+        div = random.randint(1, 7)
+        village = random.choice(self.villages[self.divisions[div]])
+        thana = random.choice(self.thanas[self.divisions[div]])
+        dist = random.choice(self.districts[div])
+        division = self.divisions[div]
+        
+        return "Ward no. " + str(ward_no) +  ", Village name: " + village + \
+                ", Thana: " + thana + ", District: " + dist + ", Division: " + division + "."
+                
     def generate_DOB(self):
         return str(random.randint(1, 28)) + "/" + str(random.randint(1, 12)) + "/" + str(random.randint(1950, 1999))
 
--- main.py
+++ main.py
@@ -11,17 +11,37 @@ if __name__ == "__main__":
     for religion in loader.religion:
         for gender in loader.gender:
             firstnames, middlenames, lastnames = loader.get_names_by_religion_gender(religion, gender)
-                
+            
             for i in range(2):
                 if i % 2 == 0:
                     if gender == 'unisex':
-                        gender = 'male'
-                    generator.generate_profile(firstnames, None, lastnames, gender, religion)
+                        gender1 = 'male'
+                        firstnames1, middlenames1 , lastnames1 = loader.get_names_by_religion_gender(religion, gender1)
+                        first_names1 = firstnames + firstnames1
+                        last_names1 = lastnames + lastnames1 
+                        generator.generate_profile(first_names1, None, last_names1, gender1, religion)
+                        
+                        gender2 = 'female'
+                        firstnames2, middlenames2 , lastnames2 = loader.get_names_by_religion_gender(religion, gender2)
+                        first_names2 = firstnames + firstnames2
+                        last_names2 = lastnames + lastnames2
+                        generator.generate_profile(first_names2, None, last_names2, gender2, religion)
                 else:
                     if gender == 'unisex':
-                        gender = 'female'
-                    generator.generate_profile(firstnames, middlenames, lastnames, gender, religion)
-            
+                        gender3 = 'male'
+                        firstnames3, middlenames3 , lastnames3 = loader.get_names_by_religion_gender(religion, gender3)
+                        first_names3 = firstnames + firstnames3
+                        middle_names3 = middlenames + middlenames3 
+                        last_names3 = lastnames + lastnames3
+                        generator.generate_profile(first_names3, middle_names3, last_names3, gender3, religion)
+                        
+                        gender4 = 'female'
+                        firstnames4, middlenames4 , lastnames4 = loader.get_names_by_religion_gender(religion, gender4)
+                        first_names4 = firstnames + firstnames4
+                        middle_names4 = middlenames + middlenames4
+                        last_names4 = lastnames + lastnames4
+                        generator.generate_profile(first_names4, middle_names4, last_names4, gender4, religion)
+                        
     data, collumns = generator.get_data()
     filename = 'new.csv'
     saved_data = loader.save_csvfile(filename, data, collumns)
