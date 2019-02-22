df6f0a1120dd74ed09626f1351de1dde36b1e3df
MuizZer07 <muizxzer07@gmail.com>
2019-02-22 08:56:54 +0600

    large dataset removed from repo

--- data_generator.py
+++ data_generator.py
@@ -40,17 +40,6 @@ class Data_Generator():
             5: '016'
         }
 
-        self.new_generated_names = []
-        self.new_generated_sex = []
-        self.new_generated_religion = []
-        self.new_generated_marital_status = []
-        self.new_generated_profession = []
-        self.new_generated_DOB = []
-        self.new_generated_emails = []
-        self.new_generated_mobile_no = []
-        self.new_generated_permanent_address = []
-        self.new_generated_present_address = []
-
         self.divisions = {
            1: "Barisal",
            2: "Chittagong",
@@ -95,8 +84,47 @@ class Data_Generator():
             'Sylhet' : ['Jahidpur', 'Sujaul', 'Balaganj', 'Jotyintapur', 'Sagarnal '],
         }
         
+        self.default_female_names = ['Afsana Ara Hoque', 'Khadija begum', 'Maimuna Haque', \
+                                     'Nafisa Kabir', 'Rehnuma Haque']
+        self.default_male_names = ['Ahmed Kamal', 'Kamal Ahmed', 'Kamal Haque', 'Jamal Ahmed', 'Jamal Haque']
+        
+        self.new_generated_names = []
+        self.new_generated_sex = []
+        self.new_generated_religion = []
+        self.new_generated_marital_status = []
+        self.new_generated_profession = []
+        self.new_generated_DOB = []
+        self.new_generated_emails = []
+        self.new_generated_mobile_no = []
+        self.new_generated_permanent_address = []
+        self.new_generated_present_address = []
+        self.new_generated_fathers_name = []
+        self.new_generated_mothers_name = []
+        self.new_generated_place_of_birth = []
+        self.new_generated_NID = []
+        
+        for i in range(5):
+            self.new_generated_names.append("Ahmed Reza Haque")
+            self.new_generated_NID.append(self.generate_NID())
+            self.new_generated_sex.append("male")
+            self.new_generated_religion.append("muslim")
+            self.new_generated_marital_status.append("Single")
+            self.new_generated_profession.append("Student")
+            self.new_generated_DOB.append("01/01/1990")
+            self.new_generated_emails.append("reza.ahmed@gmail.com")
+            self.new_generated_mobile_no.append(self.generate_phone_no())
+            self.new_generated_permanent_address.append(self.generate_permanent_address())
+            self.new_generated_present_address.append(self.generate_present_address())
+            self.new_generated_fathers_name.append(self.default_male_names[i])
+            self.new_generated_mothers_name.append(self.default_female_names[i])
+            self.new_generated_place_of_birth.append(random.choice(self.districts[random.randint(1, 4)]))
+
+
     def get_data(self):
         Data = {'Name': self.new_generated_names,
+                'NID': self.new_generated_NID,
+                'Fathers Name': self.new_generated_fathers_name,
+                'Mothers Name': self.new_generated_mothers_name,
                 'Sex': self.new_generated_sex,
                 'Religion': self.new_generated_religion,
                 'Marital Status': self.new_generated_marital_status,
@@ -106,39 +134,66 @@ class Data_Generator():
                 'Mobile No. (+880)': self.new_generated_mobile_no,
                 'Permanent Address': self.new_generated_permanent_address,
                 'Present Address': self.new_generated_present_address,
+                'Place of Birth': self.new_generated_place_of_birth,
                 }
-        columns = ['Name', 'Sex', 'Religion', 'Marital Status', 'Profession', 'Date of Birth', \
-                   'Email Address', 'Mobile No. (+880)', 'Permanent Address', 'Present Address']
+        columns = ['Name', 'NID', 'Fathers Name', 'Mothers Name', 'Sex', 'Religion', 'Marital Status', 'Profession', 'Date of Birth', \
+                   'Email Address', 'Mobile No. (+880)', 'Permanent Address', 'Present Address', 'Place of Birth']
 
         return Data, columns
 
     def generate_profile(self, first_names, middle_names, last_names, gender, religion):
+        num = random.randint(5, 15)
         for firstname in first_names:
             for lastname in last_names:
                 if middle_names != None:
                     for middlename in middle_names:
-                        self.new_generated_names.append(firstname + " " + middlename + " " + lastname)
-                        self.new_generated_sex.append(gender)
-                        self.new_generated_religion.append(religion)
-                        self.new_generated_marital_status.append(self.marital_status[random.randint(1, 4)])
-                        self.new_generated_profession.append(self.profession[random.randint(1, 12)])
-                        self.new_generated_DOB.append(self.generate_DOB())
-                        self.new_generated_emails.append(firstname.lower() + "." + lastname.lower() + self.email[random.randint(1, 3)])
-                        self.new_generated_mobile_no.append(self.generate_phone_no())
-                        self.new_generated_permanent_address.append(self.generate_permanent_address())
-                        self.new_generated_present_address.append(self.generate_present_address())
+                        for i in range(num):
+                            self.generate_single_profile(first_names, last_names, firstname, middlename, lastname, gender, religion)
+#                        
                 else:
-                    self.new_generated_names.append(firstname + " " + lastname)
-                    self.new_generated_sex.append(gender)
-                    self.new_generated_religion.append(religion)
-                    self.new_generated_marital_status.append(self.marital_status[random.randint(1, 4)])
-                    self.new_generated_profession.append(self.profession[random.randint(1, 12)])
-                    self.new_generated_DOB.append(self.generate_DOB())
-                    self.new_generated_emails.append(firstname.lower() + "." + lastname.lower() + self.email[random.randint(1, 3)])
-                    self.new_generated_mobile_no.append(self.generate_phone_no())
-                    self.new_generated_permanent_address.append(self.generate_permanent_address())
-                    self.new_generated_present_address.append(self.generate_present_address())
-                
+                    for i in range(num):
+                        self.generate_single_profile(first_names, last_names, firstname, None, lastname, gender, religion)
+             
+    def generate_single_profile(self, first_names, last_names, firstname, middlename, lastname, gender, religion):
+        if middlename is None:
+            self.new_generated_names.append(firstname + " " + lastname)
+        else:
+            self.new_generated_names.append(firstname + " " + middlename + " " + lastname)
+                 
+        self.new_generated_NID.append(self.generate_NID())
+        self.new_generated_sex.append(gender)
+        self.new_generated_religion.append(religion)
+        self.new_generated_marital_status.append(self.marital_status[random.randint(1, 4)])
+        self.new_generated_profession.append(self.profession[random.randint(1, 12)])
+        self.new_generated_DOB.append(self.generate_DOB())
+        self.new_generated_emails.append(firstname.lower() + "." + lastname.lower() + self.email[random.randint(1, 3)])
+        self.new_generated_mobile_no.append(self.generate_phone_no())
+        self.new_generated_permanent_address.append(self.generate_permanent_address())
+        self.new_generated_present_address.append(self.generate_present_address())
+        self.new_generated_fathers_name.append(self.get_fathers_name(first_names, "", lastname, gender))
+        self.new_generated_mothers_name.append(self.get_mothers_name(first_names, last_names, gender))
+        self.new_generated_place_of_birth.append(random.choice(self.districts[random.randint(1, 4)]))
+        
+    
+    def get_fathers_name(self, firstnames, middlename, lastname, gender):
+        fathers_name = None
+        if gender is 'male':
+            fathers_name = random.choice(firstnames) + " " + middlename + " " + lastname
+        else:
+            fathers_name = random.choice(self.new_generated_fathers_name)
+        
+        return fathers_name
+        
+    def get_mothers_name(self, firstnames, lastnames, gender):
+        mothers_name = None
+        if gender is 'female':
+            mothers_name = random.choice(firstnames) + " " + random.choice(lastnames)
+        else:
+            if len(self.new_generated_mothers_name) > 0 :
+                mothers_name = random.choice(self.new_generated_mothers_name)
+            
+        return mothers_name
+        
     def generate_present_address(self):
         house_no = random.randint(1, 70)
         road_no = random.randint(1, 20)
@@ -170,5 +225,10 @@ class Data_Generator():
         str1 = str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) + \
                str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) + \
                str(random.randint(0, 9)) + str(random.randint(0, 9))
+    
+    def generate_NID(self):
+        str1 = str(random.randint(1, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) + \
+               str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) + \
+               str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
 
-        return self.mobile_no[random.randint(1, 3)] + str1
+        return str1
